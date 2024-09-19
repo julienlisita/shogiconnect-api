e71ce97305217efbdc47b415ec17fee017fd5f3e
jlisita <lisita_julien@hotmail.fr>
2024-09-19 19:49:11 +0200

    update categories.js with 6 entries

--- db/categories.js
+++ db/categories.js
@@ -1,24 +1,34 @@
-const categories = [
+const categories = 
+[
     {
-        id: 1,
-        title: "Stratégies et tactiques",
-        description: "Discutez des stratégies et des tactiques avancées du shogi, partagez vos ouvertures préférées, et analysez des parties célèbres.",
+        id: 1, 
+        title: "Le shogi en général", 
+        description: "Discutez de tout les sujets à propos du Shogi" 
     },
-    {
-        id: 2,
-        title: "Débutants",
-        description: "Un espace pour les nouveaux joueurs pour poser des questions, apprendre les bases, et obtenir des conseils pour commencer à jouer au shogi",
+    { 
+        id: 2, 
+        title: "Pour les débutants", 
+        description: "Posez vos questions sur l’apprentissage du Shogi" 
     },
-    {
-        id: 3,
-        title: "Actus et Tournois",
-        description: "Mises à jour sur les derniers tournois de shogi, les nouvelles du monde du shogi, et les annonces importantes concernant le shogi.",
+    {   id: 3,
+        title: "Analyse de partie", 
+        description: "Analysez votre jeu avec la communauté" 
     },
-    {
-        id: 4,
-        title: "Culture et Histoire",
-        description: "Explorez l'histoire et la culture du shogi, y compris les origines du jeu, son évolution, et son impact culturel au Japon et ailleurs.",
+    { 
+        id: 4, 
+        title: "Clubs et tournois", 
+        description: "Échangez sur les clubs et les tournois en cours" 
+    },
+    { 
+        id: 5, 
+        title: "Feedback sur le site", 
+        description: "Signalez les bugs, vos demandes et suggestions" 
+    },
+    { 
+        id: 6, 
+        title: "Hors sujet", 
+        description: "Tout ce qui ne concerne pas le Shogi" 
     }
-];
+]
 
 module.exports = categories
